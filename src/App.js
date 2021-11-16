f45c060da5a5ea6b4eca10ec42b3a03df2bd67fd
sarahandreas <sarah.andreas@gmail.com>
2021-11-16 11:03:59 +1100

    trying things

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './reset.css'
 import './base.css'
 import './general.css'
+import './page-transition.css'
 
 import Nav from './Components/Blocks/Nav';
 
-import { Switch, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 import Home from './Components/Pages/Home'
 import Destinations from './Components/Pages/Destinations'
 import Crew from './Components/Pages/Crew';
 import Tech from './Components/Pages/Tech';
+
+import { CSSTransition } from 'react-transition-group';
 
 import { useState } from 'react';
 
@@ -20,20 +23,57 @@ function App() {
   return (
     <div className={`App ${page}`}>
       <Nav funcs={[page, setPage]}/>
-      <Switch>
-        <Route exact path='/'>
-          <Home setPage={setPage}/>
-        </Route>
-        <Route path='/destinations'>
-          <Destinations />
-        </Route>
-        <Route path='/crew'>
-          <Crew />
-        </Route>
-        <Route path='/technology'>
-          <Tech />
-        </Route>
-      </Switch>
+      {/* <Route exact path='/'>
+        <Home setPage={setPage}/>
+      </Route> */}
+      <Route path='/destinations'>
+      {({ match }) => (
+          <CSSTransition
+            in={match != null}
+            timeout={300}
+            classNames="page"
+            unmountOnExit={true}
+          >
+            <Destinations />
+          </CSSTransition>
+        )}
+      </Route>
+      <Route path='/crew'>
+      {({ match }) => (
+          <CSSTransition
+            in={match != null}
+            timeout={300}
+            classNames="page"
+            unmountOnExit={true}
+          >
+            <Crew />
+          </CSSTransition>
+        )}
+      </Route>
+      <Route path='/technology'>
+      {({ match }) => (
+          <CSSTransition
+            in={match != null}
+            timeout={300}
+            classNames="page"
+            unmountOnExit={true}
+          >
+            <Tech />
+          </CSSTransition>
+        )}
+      </Route>
+      <Route exact path='/'>
+        {({ match }) => (
+          <CSSTransition
+            in={match != null}
+            timeout={300}
+            classNames="page"
+            unmountOnExit={true}
+          >
+            <Home setPage={setPage}/>
+          </CSSTransition>
+        )}
+      </Route>
     </div>
   );
 }
